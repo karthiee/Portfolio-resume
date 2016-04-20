3ec9ad1caeb1ae92e06be6b6de417d5ab0a1d564
karthiee <karthi476oct@gmail.com>
2016-04-20 15:01:47 +0530

    Added files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+	$(document).ready(function(){
+		$('#hidden_box1').hide();
+			$('#knowmore1').click(function(){
+				$('#hidden_box1').fadeIn();
+			});
+			$('#hidden_box2').hide();
+			$('#knowmore2').click(function(){
+						$('#hidden_box2').fadeIn();
+
+			});
+	});
